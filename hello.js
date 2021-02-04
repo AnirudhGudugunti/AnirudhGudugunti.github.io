c661da9c4540b9de896d7766974051d80a917268
Anirudh Gudugunti <53473025+AnirudhGudugunti@users.noreply.github.com>
2021-02-04 16:35:07 +0530

    Test_Upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+/*alert('hello')*/
+<script>
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+}
+</script>
